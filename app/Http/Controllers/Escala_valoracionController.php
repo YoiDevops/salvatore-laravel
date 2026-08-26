e2ea6b90da6febfc6e3d82de568c0f64faa4690b
Anfixsinax <CristianDevVFIX@gmail.com>
2026-08-26 10:16:26 -0500

    Migraciones completas con models, creacion de archivos controllers

---
+++ app/Http/Controllers/Escala_valoracionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Escala_valoracionController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Indicador_logroController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Indicador_logroController extends Controller
+{
+    //
+}
